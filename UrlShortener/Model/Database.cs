0ba7fbd1b99a278da9758fc49c99d6d1ea424a5a
Dudus <kamildudek1@gmail.com>
2025-06-20 18:59:17 +0200

    Added EF Core

---
+++ UrlShortener/Model/Database.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Model
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Shortener> Shortener { get; set; }
+    }
+}
---
+++ UrlShortener/Model/Shortener.cs
@@ -0,0 +1,10 @@
+﻿namespace UrlShortener.Model
+{
+    public class Shortener
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+        public DateTime? TimeToLive { get; set; }
+    }
+}

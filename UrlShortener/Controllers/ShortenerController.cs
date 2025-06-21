5e8c0194ecb7230c25ea1c92421e2312c47f99bc
Dudus <kamildudek1@gmail.com>
2025-06-21 13:17:11 +0200

    Added GenShortUrl Algorithm

--- UrlShortener/Controllers/ShortenerController.cs
+++ UrlShortener/Controllers/ShortenerController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using System;
 using UrlShortener.Model;
+
 using ILogger = Serilog.ILogger;
 
 namespace UrlShortener.Controllers
@@ -23,7 +26,12 @@ namespace UrlShortener.Controllers
         [HttpPost("ShortenUrl")]
         public async Task<ActionResult<string>> ShortenUrl(string url, DateTime? timeToLive = null)
         {
-            var shortUrl = String.Empty;
+            var shortUrl = await ShortUrlWithoutDuplication();
+            
+            if(shortUrl == String.Empty)
+            {
+                return NotFound();
+            }
 
             var shortener = new Shortener()
             {
@@ -31,22 +39,22 @@ namespace UrlShortener.Controllers
                 ShortUrl = shortUrl,
                 TimeToLive = timeToLive,
             };
-            await _context.AddAsync<Shortener>(shortener);
-            var result = await _context.SaveChangesAsync();
 
-            if(result > 0)
-            {
-                _logger.Information($"Added new Url {url} as {shortUrl}, ttl to {timeToLive}");
-                return Ok(shortUrl);
-            }
-
-            return NotFound();
+            return await CreateShortUrl(shortener);
         }
 
         [HttpPost("ShortenUrlWithShortId")]
         public ActionResult<string> ShortenUrlWithShortId(string url, string shortId, DateTime? timeToLive = null)
         {
-            _logger.Information("Test");
+            //_logger.Information(GenShortUrl());
+
+            List<string> gen = new List<string>();
+            for (var i = 0; i < 1000000; i++)
+            {
+                gen.Add(GenShortUrl());
+            }
+
+            _logger.Information($"count list: {gen.Count()} vs {gen.Distinct().Count()}");
 
             return "";
         }
@@ -66,6 +74,67 @@ namespace UrlShortener.Controllers
 
             return "";
         }
+
+        #region Helpers
+
+        private string GenShortUrl(int minLength = 5)
+        {
+            string result = String.Empty;
+
+            char[] arrayOfAlphanumericals = [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n',
+                'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                ];
+
+            var random = new Random();
+
+            for (int i = 0; i < minLength; i ++)
+            {
+                int randomNumber = random.Next(0, arrayOfAlphanumericals.Length);
+                result += arrayOfAlphanumericals[randomNumber];
+            }
+            
+            return result;
+        }
+
+        private async Task<string> ShortUrlWithoutDuplication()
+        {
+            int nextGenAttempts = 10;
+
+            var result = String.Empty;
+            for (int i = 0; i < nextGenAttempts; i++)
+            {
+                var genUrl = GenShortUrl();
+                var elementExists = await _context.Shortener.FirstOrDefaultAsync<Shortener>(x => x.ShortUrl == genUrl);
+
+                if (elementExists == null)
+                {
+                    result = genUrl;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private async Task<ActionResult<string>> CreateShortUrl(Shortener shortener)
+        {
+            await _context.AddAsync<Shortener>(shortener);
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                _logger.Information($"Added new Url {shortener.Url} as {shortener.ShortUrl}, ttl to {shortener.TimeToLive}");
+                return Ok(shortener.ShortUrl);
+            }
+
+            return NotFound();
+        }
+
+        #endregion
 
     }
 }
